8139c4a969d967ff31b907bf62aa651cd5f588e2
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2021-12-14 21:43:11 -0300

    Primeiro commit de criação do projeto

---
+++ src/main/java/com/digitalinovatioone/cervejaria/CervejariaApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinovatioone.cervejaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CervejariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CervejariaApplication.class, args);
+	}
+
+}
