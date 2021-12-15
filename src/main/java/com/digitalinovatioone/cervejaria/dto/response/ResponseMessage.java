759f07319de9f4da6795e846808482efeb691f40
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2021-12-15 17:49:55 -0300

    Adiciona classe responseMessage para retornos com texto da api

---
+++ src/main/java/com/digitalinovatioone/cervejaria/dto/response/ResponseMessage.java
@@ -0,0 +1,10 @@
+package com.digitalinovatioone.cervejaria.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ResponseMessage {
+    private String message;
+}
