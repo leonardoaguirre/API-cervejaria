c76162fc6b91d2f6938f6f877377612cd4c7b905
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2021-12-16 19:32:53 -0300

    Refatora dependencias e excecao de bebida nao encontrada

--- src/main/java/com/digitalinovatioone/cervejaria/exception/BebidaNaoEncontradaException.java
+++ src/main/java/com/digitalinovatioone/cervejaria/exception/BebidaNaoEncontradaException.java
@@ -3,7 +3,7 @@ package com.digitalinovatioone.cervejaria.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class BebidaNaoEncontradaException extends Exception{
     public BebidaNaoEncontradaException(String nomeBebida){
         super(String.format("Bebida com o nome %s não foi encontrada no sistema!", nomeBebida));
